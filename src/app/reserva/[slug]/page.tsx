3bb6d717144a79f75b26f24af6a72281577c1edf
leandro orsenigo <leandroorsenigo@gmail.com>
2026-03-16 15:37:04 -0300

    fix: forzar ruta dinamica en reserva/[slug]

--- src/app/reserva/[slug]/page.tsx
+++ src/app/reserva/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import BookingClient from "./BookingClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function PublicBookingPage({
   params,
 }: {
